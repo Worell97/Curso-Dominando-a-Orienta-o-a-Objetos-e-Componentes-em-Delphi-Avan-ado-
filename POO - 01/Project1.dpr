06d00425340e41c815ab628cd54fb7f76679a653
Worell97 <46696558+Worell97@users.noreply.github.com>
2020-01-13 09:44:15 -0200

    Add files via upload

---
+++ POO - 01/Project1.dpr
@@ -0,0 +1,14 @@
+﻿program Project1;
+
+uses
+  Vcl.Forms,
+  Principal in 'Principal.dpr' {Form1};
+
+{$R *.res}
+
+begin
+  Application.Initialize;
+  Application.MainFormOnTaskbar := True;
+  Application.CreateForm(TForm1, Form1);
+  Application.Run;
+end.
---
+++ POO - 02/Project1.dpr
@@ -0,0 +1,15 @@
+﻿program Project1;
+
+uses
+  Vcl.Forms,
+  produto in 'produto.pas',
+  Principal in 'Principal.pas' {Form1};
+
+{$R *.res}
+
+begin
+  Application.Initialize;
+  Application.MainFormOnTaskbar := True;
+  Application.CreateForm(TForm1, Form1);
+  Application.Run;
+end.
